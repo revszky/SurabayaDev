d3fe19579a2fb453d790e82edc44085a3695243f
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-04-23 14:03:58 +0700

    WIP

--- src/app/components/home/Event.tsx
+++ src/app/components/home/Event.tsx
@@ -1,6 +1,37 @@
 import { IconCoin, IconMapPin } from "@tabler/icons-react";
 import React from "react";
 
+const eventsData = [
+  {
+    imageSrc: "/event/event1.jpeg",
+    title:
+      "Designing for Success: UI/UX Freelance and the Business Landscape (2023)",
+    date: "05 Agustus 2023",
+    description: "Surabaya Dev Tech Community Revolution!",
+    map: "Offline Event",
+    price: "Gratis",
+  },
+  {
+    imageSrc: "/event/event2.jpeg",
+    title:
+      "Breaking the Barrier: Unleashing the Power of Frontend & FullStack Web Dev",
+    date: "24 Juni 2023",
+    description:
+      "Bersiap untuk hadir di SurabayaDev Offline Meetup! Breaking the Barrier: Unleashing the Power of Frontend and FullStack Web Development",
+    map: "Offline Event",
+    price: "Gratis",
+  },
+  {
+    imageSrc: "/event/event3.jpeg",
+    title: "Web Automation Testing with Puppeteer",
+    date: "01 April 2023",
+    description:
+      "Learn the basics of web automation testing using Puppeteer, an open-source Node library, in this webinar. Boost your testing efficiency!",
+    map: "Offline Event",
+    price: "Gratis",
+  },
+];
+
 const Event = () => {
   return (
     <div className="flex flex-col items-center justify-center">
@@ -12,45 +43,47 @@ const Event = () => {
       </div>
 
       <div className="space-y-8">
-        <div className="flex flex-col md:flex-row items-center md:gap-4 rounded-2xl bg-gray-200">
-          <div className="rounded-t-2xl md:rounded-t-none md:rounded-l-2xl overflow-hidden">
-            <img
-              src="/event/event1.jpeg"
-              alt="event"
-              className="object-cover"
-            />
-          </div>
-
-          <div className="flex flex-col items-start p-4">
-            <div className="flex items-center gap-6">
-              <div className="flex items-center gap-1 border-2 border-black p-1.5 rounded-2xl">
-                <IconMapPin className="w-5 h-5" />
-                <p className="text-sm">Offline Event</p>
-              </div>
-
-              <div className="flex items-center gap-1 border-2 border-black p-1.5 rounded-2xl">
-                <IconCoin className="w-5 h-5" />
-                <p className="text-sm">Gratis</p>
-              </div>
+        {eventsData.map((event, index) => (
+          <div
+            key={index}
+            className="flex flex-col md:flex-row items-center md:gap-4 rounded-2xl bg-gray-200"
+          >
+            <div className="rounded-t-2xl md:rounded-t-none md:rounded-l-2xl overflow-hidden">
+              <img
+                src={event.imageSrc}
+                alt="event"
+                className="object-cover w-full h-full"
+              />
             </div>
 
-            <div className="max-w-3xl py-4">
-              <div className="py-2">
-                <h1 className="font-semibold text-xl">
-                  Designing for Success: UI/UX Freelance and the Business
-                  Landscape (2023)
-                </h1>
-                <p className="text-sm">05 Agustus 2023</p>
+            <div className="flex flex-col items-start p-6">
+              <div className="flex items-center gap-6">
+                <div className="flex items-center gap-1 border-2 border-black p-1.5 rounded-2xl">
+                  <IconMapPin className="w-5 h-5" />
+                  <p className="text-sm">{event.map}</p>
+                </div>
+
+                <div className="flex items-center gap-1 border-2 border-black p-1.5 rounded-2xl">
+                  <IconCoin className="w-5 h-5" />
+                  <p className="text-sm">{event.price}</p>
+                </div>
               </div>
 
-              <p>Surabaya Dev Tech Community Revolution!</p>
-            </div>
+              <div className="max-w-3xl py-4">
+                <div className="py-2">
+                  <h1 className="font-semibold text-xl">{event.title}</h1>
+                  <p className="text-sm">{event.date}</p>
+                </div>
 
-            <button className="px-4 py-1 bg-gray-300 rounded-full">
-              Detail Event
-            </button>
+                <p>{event.description}</p>
+              </div>
+
+              <button className="px-4 py-1 bg-gray-300 rounded-full">
+                Detail Event
+              </button>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
